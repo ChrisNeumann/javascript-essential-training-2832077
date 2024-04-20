9b37cd11ac17d2bc5594b73850be038095f43786
ChrisNeumann <chr_neumann@t-online.de>
2024-04-20 11:23:49 +0200

    Practice challenge solution

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,22 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const telephone = {
+  name: "My Telephone",
+  type: "iPhone",
+  touchscreen: true,
+  batteryCharge: 0.8,
+  numCameras: 2,
+  posCameras: {
+    cam1: "rear",
+    cam2: "front",
+  },
+};
+
+console.log("Telephone object is:", telephone);
+console.log(
+  "The battery charge of the telephone is:",
+  telephone.batteryCharge * 100,
+  "%"
+);
