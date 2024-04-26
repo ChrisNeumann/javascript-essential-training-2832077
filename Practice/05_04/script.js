0ec3e4f17fe750f52af25d7619dc01f834fb5af8
ChrisNeumann <chr_neumann@t-online.de>
2024-04-26 08:25:11 +0200

    practice solution

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,14 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+let backpack = document.querySelectorAll(".backpack");
+backpack.forEach((elem) =>
+  elem.querySelector(".lid-toggle").addEventListener("click", () => {
+    let lid = elem.querySelector(".backpack__lid span");
+    if (lid.innerHTML == "closed") {
+      lid.innerHTML = "open";
+    } else {
+      lid.innerHTML = "closed";
+    }
+  })
+);
