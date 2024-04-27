c63f7391546db6834a483d00ffc34c0c39941464
ChrisNeumann <chr_neumann@t-online.de>
2024-04-27 16:20:23 +0200

    Practice Solution

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,52 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+let backpackForms = document.querySelectorAll(".backpack__strap form");
+
+backpackForms.forEach((elem) => {
+  elem.classList.add("lengthEdit");
+});
+backpackForms.forEach((elem) => {
+  elem.classList.add("backpack__starp__editForm");
+});
+
+backpackForms.forEach((elem) => {
+  elem.classList.remove("backpack__starp__editForm");
+});
+
+backpackForms.forEach((elem) => {
+  if (elem.parentElement.getAttribute("data-side") == "left") {
+    elem.setAttribute("name", "leftStrap");
+  } else {
+    elem.setAttribute("name", "rightStrap");
+  }
+});
+
+backpackForms.forEach((elem) => {
+  console.log(
+    "Name Attribute of " +
+      elem.parentElement.getAttribute("Data-side") +
+      " strap form element of the " +
+      elem.parentElement.parentElement.parentElement.querySelector(
+        ".backpack__name"
+      ).innerHTML +
+      " is " +
+      elem.getAttribute("name")
+  );
+});
+
+backpackForms.forEach((elem) => {
+  switch (elem.getAttribute("name")) {
+    case "leftStrap":
+      elem.setAttribute("name", "leftStrapForm");
+      break;
+    case "rightStrap":
+      elem.setAttribute("name", "rightStrapForm");
+      break;
+  }
+});
+
+document.querySelectorAll(".backpack__name").forEach((elem) => {
+  elem.style.backgroundColor = "blue";
+  elem.style.color = "white";
+});
