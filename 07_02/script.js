f84744c40d3b99ecbe87aca72238ee126a8829f4
ChrisNeumann <chr_neumann@t-online.de>
2024-05-08 15:22:49 +0200

    course progression

--- 07_02/script.js
+++ 07_02/script.js
@@ -8,3 +8,9 @@ let item = "flashlight";
 const collection = ["Piggy", item, 5, true];
 
 console.log(collection);
+
+collection[2] = "camera"; // change contens at any position
+collection[collection.length] = "new item"; // add a new item at the end
+collection[9] = "at the end"; // add a nw item at any position , regardless of actual length - unused positions stay undefined
+console.log(collection[8]); // demonstrates the undefined content of a slot not assigned a value
+console.log(collection);
