b8ec4a0744a07da23fea3788a687796dad080ca1
ChrisNeumann <chr_neumann@t-online.de>
2024-05-09 14:48:46 +0200

    practice solution

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,39 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+function mainFunction() {
+  const mainHTML = document.querySelector("main");
+  const newArticle = document.createElement("article");
+  newArticle.classList.add("newArticle");
+  newArticle.innerHTML = `<h1> New Topic </h1>`;
+  mainHTML.append(newArticle);
+}
+
+mainFunction();
+
+const classFunction = function () {
+  const greenPack = {
+    name: "Frog Pack",
+    color: "green",
+    volume: 8,
+    pocketNum: 3,
+  };
+  const greyPack = {
+    name: "My Pack",
+    color: "grey",
+    volume: 5,
+    pocketNum: 2,
+  };
+  return [greenPack, greyPack];
+};
+
+classFunction().forEach((backpack) => {
+  const article = document.querySelector(".newArticle");
+  const newList = document.createElement("ul");
+  newList.innerHTML = `<li>Volume: ${backpack.volume}</li>
+      <li>Color: ${backpack.color}</li>
+      <li>Number of pockets: ${backpack.pocketNum}</li>
+    `;
+  article.append(newList);
+});
